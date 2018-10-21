176317d4d7be2f3b8c65c6b5f38cc9827a1af555
Steve Purcell <steve@sanityinc.com>
2018-10-21 17:33:25 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+[![Melpa Status](http://melpa.org/packages/ns-auto-titlebar-badge.svg)](http://melpa.org/#/ns-auto-titlebar)
+[![Melpa Stable Status](http://stable.melpa.org/packages/ns-auto-titlebar-badge.svg)](http://stable.melpa.org/#/ns-auto-titlebar)
+<a href="https://www.patreon.com/sanityinc"><img alt="Support me" src="https://img.shields.io/badge/Support%20Me-%F0%9F%92%97-ff69b4.svg"></a>
+
+ns-auto-titlebar.el
+===================
+
+Provides a global minor mode, `ns-auto-titlebar-mode` which - when
+enabled - keeps the "ns-appearance" frame parameter correctly set in
+GUI frames so that it matches the currently-enabled theme, whether it
+is light or dark.
+
+Installation
+=============
+
+If you choose not to use one of the convenient
+packages in [MELPA][melpa], you'll need to
+add the directory containing `ns-auto-titlebar.el` to your `load-path`, and
+then `(require 'ns-auto-titlebar)`.
+
+Usage
+=====
+
+Enable `ns-auto-titlebar-mode` with `M-x customize`, or like this:
+
+```lisp
+(when (eq system-type 'darwin) (ns-auto-titlebar-mode))
+```
+Note that it is safe to omit the "when" condition if you prefer.
+
+[melpa]: http://melpa.org
+
+<hr>
+
+
+[💝 Support this project and my other Open Source work](https://www.patreon.com/sanityinc)
+
+[💼 LinkedIn profile](https://uk.linkedin.com/in/stevepurcell)
+
+[✍ sanityinc.com](http://www.sanityinc.com/)
+
+[🐦 @sanityinc](https://twitter.com/sanityinc)
---
+++ ns-auto-titlebar.el
@@ -0,0 +1,66 @@
+;;; ns-auto-titlebar.el --- Set the MacOS transparent titlebar to match theme  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Steve Purcell
+
+;; Author: Steve Purcell <steve@sanityinc.com>
+;; Keywords: frames
+;; Homepage: https://github.com/purcell/ns-auto-titlebar
+;; Version: 0
+;; Package-Requires: ((emacs "24.4"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Provides a global minor mode, `ns-auto-titlebar-mode' which - when
+;; enabled - keeps the "ns-appearance" frame parameter correctly set
+;; in GUI frames so that it matches the currently-enabled theme,
+;; whether it is light or dark.
+
+;; Usage:
+
+;;     (when (eq system-type 'darwin) (ns-auto-titlebar-mode))
+
+;; Note that it is safe to omit the "when" condition if you prefer.
+
+;;; Code:
+
+;;;###autoload
+(define-minor-mode ns-auto-titlebar-mode
+  "Set the MacOS transparent titlebar background automatically."
+  :global t
+  :lighter ""
+  :require 'ns-auto-titlebar
+  (when ns-auto-titlebar-mode
+    (ns-auto-titlebar-set-all-frames)))
+
+(defun ns-auto-titlebar-set-frame (frame)
+  "Set ns-appearance frame parameter for FRAME to match its background-mode parameter."
+  (when ns-auto-titlebar-mode
+    (let ((mode (frame-parameter frame 'background-mode)))
+      (modify-frame-parameters frame `((ns-transparent-titlebar . t) (ns-appearance . ,mode))))))
+
+(defun ns-auto-titlebar-set-all-frames (&rest _)
+  "Set ns-appearance frame parameter for all frames to match their background-mode parameter."
+  (when ns-auto-titlebar-mode
+    (mapc 'sanityinc/set-frame-ns-appearance (frame-list))))
+
+(add-hook 'after-init-hook 'ns-auto-titlebar-set-all-frames)
+(add-hook 'after-make-frame-functions 'ns-auto-titlebar-set-frame)
+(advice-add 'load-theme :after 'ns-auto-titlebar-set-all-frames)
+(advice-add 'enable-theme :after 'ns-auto-titlebar-set-all-frames)
+
+
+(provide 'ns-auto-titlebar)
+;;; ns-auto-titlebar.el ends here
